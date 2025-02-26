48e0bfa4bbae725c363b37ed8914ffbf4e4c2620
Mariam Elmoazen <mariam.elmoazen@gmail.com>
2025-02-26 20:42:40 +0200

    updated skills

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,9 +18,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/mariam-elmoazen-b6835a172/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://leetcode.com/u/marie2233/"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/Mariam22-hub"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2025. All Rights Reserved</p>
           </Col>
